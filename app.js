33d620dde3230b65b951255ac757e6bc4f8da4ca
atakanhbk <atakanhbk@gmail.com>
2024-02-16 17:02:07 +0300

    Finish Generate Algo

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const userInput = document.getElementsByClassName("user-input")[0];
 const generateBtn = document.getElementsByClassName("generate-btn")[0];
-
+const qrImage = document.querySelector(".qr-code > img");
 
 const generateFunction = (e) => {
-    e.preventDefault();
-    
-    const userValue = userInput.value.trim();
-    
+  e.preventDefault();
+  const userValue = userInput.value.trim();
 
-    console.log(userValue);
-  
-}
+  if (userValue != "") {
+    const api = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${userValue}`;
+    qrImage.src = `${api}`;
+  } else {
+    alert("Please You Should Write Something");
+  }
+};
 
-
-generateBtn.addEventListener("click",generateFunction);
+generateBtn.addEventListener("click", generateFunction);
