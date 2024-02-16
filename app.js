0c30f7e5d2b3ea371cf2cea37fd5dd2079fda6ae
atakanhbk <atakanhbk@gmail.com>
2024-02-16 17:26:11 +0300

    Finish

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const userInput = document.getElementsByClassName("user-input")[0];
 const generateBtn = document.getElementsByClassName("generate-btn")[0];
 const qrImage = document.querySelector(".qr-code > img");
+const spinnerBorder = document.getElementsByClassName("spinner-border")[0];
 
 const generateFunction = (e) => {
   e.preventDefault();
   const userValue = userInput.value.trim();
 
   if (userValue != "") {
+    spinnerBorder.style.display = "block";
     const api = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${userValue}`;
     qrImage.src = `${api}`;
-    qrImage.parentNode.style.display = "block";
+
+    qrImage.addEventListener("load", () => {
+        qrImage.parentNode.style.display = "block";
+        spinnerBorder.style.display = "none";
+    });
   } else {
     alert("Please You Should Write Something");
   }
